d45760fe609c64e84b66f5165318e7b2fe7d810e
Andrzej Stolarek <stolar.code@gmail.com>
2021-03-07 16:42:20 +0100

    added SkillList component

---
+++ src/App/common/SkillList/index.js
@@ -0,0 +1,84 @@
+import React from 'react'
+import { StyledList, ListItem } from "../List/styled";
+
+const SkillList = () => (
+    <StyledList>
+        <ListItem>
+            Semantic &amp; accessible HTML
+        </ListItem>
+        <ListItem>
+            Responsive Web Design
+        </ListItem>
+        <ListItem>
+            Teamwork
+        </ListItem>
+        <ListItem>
+            Markdown
+        </ListItem>
+        <ListItem>
+            Immutability
+        </ListItem>
+        <ListItem>
+            Semantic &amp; accessible HTML
+        </ListItem>
+        <ListItem>
+            Responsive Web Design
+        </ListItem>
+        <ListItem>
+            Teamwork
+        </ListItem>
+        <ListItem>
+            Markdown
+        </ListItem>
+        <ListItem>
+            Immutability
+        </ListItem>
+        <ListItem>
+            Semantic &amp; accessible HTML
+        </ListItem>
+        <ListItem>
+            Responsive Web Design
+        </ListItem>
+        <ListItem>
+            Teamwork
+        </ListItem>
+        <ListItem>
+            Markdown
+        </ListItem>
+        <ListItem>
+            Immutability
+        </ListItem>
+        <ListItem>
+            Semantic &amp; accessible HTML
+        </ListItem>
+        <ListItem>
+            Responsive Web Design
+        </ListItem>
+        <ListItem>
+            Teamwork
+        </ListItem>
+        <ListItem>
+            Markdown
+        </ListItem>
+        <ListItem>
+            Immutability
+        </ListItem>
+        <ListItem>
+            Semantic &amp; accessible HTML
+        </ListItem>
+        <ListItem>
+            Responsive Web Design
+        </ListItem>
+        <ListItem>
+            Teamwork
+        </ListItem>
+        <ListItem>
+            Markdown
+        </ListItem>
+        <ListItem>
+            Immutability
+        </ListItem>
+    </StyledList>
+);
+
+export default SkillList;
