4594acb39a4be2584ac16f781314d99990192da0
Andrzej Stolarek <stolar.code@gmail.com>
2021-03-07 16:42:05 +0100

    added LearnList component

---
+++ src/App/common/LearnList/index.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { StyledList, ListItem } from "../List/styled";
+
+const LearnList = () => (
+    <StyledList>
+        <ListItem>
+            React Context
+        </ListItem>
+        <ListItem>
+            Node.js
+        </ListItem>
+        <ListItem>
+            Cypress
+        </ListItem>
+        <ListItem>
+            Unit testing
+        </ListItem>
+        <ListItem>
+            React Context
+        </ListItem>
+        <ListItem>
+            Node.js
+        </ListItem>
+    </StyledList>
+);
+
+export default LearnList;
