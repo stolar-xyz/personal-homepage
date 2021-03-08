578a12b4d2eab7271ea6ff7182f8e49b432c8875
Andrzej Stolarek <stolar.code@gmail.com>
2021-03-08 23:30:11 +0100

    added Button component

---
+++ src/App/common/Button/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { StyledButton } from './styled';
+
+const Button = () => (
+    <StyledButton>
+        🤹‍♂️ Hire Me
+    </StyledButton>
+);
+
+export default Button;
---
+++ src/App/common/Button/styled.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const StyledButton = styled.button`
+    padding: 12px 16px;
+    border: 1px solid rgba(209, 213, 218, 0.3);
+    background: #0366D6;
+    color: #ffffff;
+    cursor: pointer;
+    border-radius: 4px;
+    font-weight: 600;
+    letter-spacing: 0.05em;
+`;
