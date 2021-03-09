60c65d92feff3f20d413ead59d0fd99850af58a2
Andrzej Stolarek <stolar.code@gmail.com>
2021-03-09 23:20:55 +0100

    changed Container component to Main with different styles

---
+++ src/App/common/Main/index.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const Main = styled.main`
+    padding: 14px;
+    margin: 105px 0;
+    display: grid;
+    grid-gap: 72px;
+    grid-template-columns: minmax(auto, 1216px);
+    justify-content: center;
+`;
+
+export default Main;
