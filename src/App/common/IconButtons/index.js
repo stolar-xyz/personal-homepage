394c00cd48925dde385a0d87e2c4ea9851d9e709
Andrzej Stolarek <stolar.code@gmail.com>
2021-03-09 23:21:27 +0100

    added example IconButtons component

---
+++ src/App/common/IconButtons/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { StyledIconButton, Wrapper } from './styled';
+
+const IconButtons = () => (
+    <Wrapper>
+        <StyledIconButton>
+            click me!
+    </StyledIconButton>
+        <StyledIconButton>
+            click me!
+    </StyledIconButton>
+        <StyledIconButton>
+            click me!
+    </StyledIconButton>
+        <StyledIconButton>
+            click me!
+    </StyledIconButton>
+    </Wrapper>
+);
+
+export default IconButtons;
---
+++ src/App/common/IconButtons/styled.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+    display: flex;
+`;
+
+export const StyledIconButton = styled.button`
+    margin-right: 24px;
+    color: #ffffff;
+    background: black;
+    cursor: pointer;
+    border: none;
+`;
