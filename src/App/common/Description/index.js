e03571d990241eb087f7fe7006ca805d75d4f16a
Andrzej Stolarek <stolar.code@gmail.com>
2021-03-10 20:49:45 +0100

    added new styles to Description component

--- src/App/common/Description/index.js
+++ src/App/common/Description/index.js
@@ -5,12 +5,19 @@ const Description = styled.p`
     font-size: 20px;
     color: #6E7E91;
     line-height: 1.4;
+    max-width: 633px;
 
     ${({ contact }) => contact && css`
         color: black;
         font-size: 18px;
         margin: 0 0 56px 0;
         max-width: 670px;
+    `}
+
+    ${({ tile }) => tile && css`
+        font-size: 18px;
+        margin: 24px 0;
+        max-width: none;
     `}
 `;
 
