bad3f46c8590308e062e89524049dbf2e63c47bf
Andrzej Stolarek <stolar.code@gmail.com>
2021-03-10 20:56:56 +0100

    builded App structure using created components

--- src/App/App.js
+++ src/App/App.js
@@ -1,20 +1,84 @@
-import { Container } from "./common/Container/styled";
+import Article from "./common/Article";
+import Button from "./common/Button";
+import Main from "./common/Main";
+import IconButtons from "./common/IconButtons";
 import LearnList from "./common/LearnList";
+import Profile from "./common/Profile";
 import Section from "./common/Section";
 import SkillList from "./common/SkillList";
+import TilesSection from "./common/TilesSection";
+import Description from "./common/Description";
+import Title from "./common/Title";
 
 function App() {
   return (
-    <Container>
+    <Main>
+      <Article
+        image={<Profile />}
+        extraTitleContent={"This is"}
+        title={
+          <Title>
+            {"Andrzej Stolarek"}
+          </Title>
+        }
+        description={
+          <Description>
+            {"👨🏻‍💻 I’m a passionate Frontend Developer in love with React, currently looking for new job opportunities."}
+          </Description>
+        }
+        extraContent={<Button />}
+      />
       <Section
-        title={"My skillset includes 🛠️"}
+        title={
+          <Title
+            subtitle
+            as="h2"
+          >
+            {"My skillset includes 🛠️"}
+          </Title>
+        }
         body={<SkillList />}
       />
       <Section
-        title={"What I want to learn next 🚀"}
+        title={
+          <Title
+            subtitle
+            as="h2"
+          >
+            {"What I want to learn next 🚀"}
+          </Title>
+        }
         body={<LearnList />}
       />
-    </Container>
+      <TilesSection
+        extraContent={"Github Icon"}
+        title={
+          <Title
+            subtitle
+            as="h2"
+          >
+            {"Portfolio"}
+          </Title>
+        }
+        extraTitleContent={"My recent projects"}
+      />
+      <Article
+        extraTitleContent={"Let’s talk!"}
+        title={
+          <Title
+            mail
+            as="p">
+            {"stolar.code@gmail.com"}
+          </Title>
+        }
+        description={
+          <Description contact>
+            {"I’m always open to new projects whenever I have the time. If you have a website, dashboard or mobile app in mind and need some help to make your ideas come to life, feel free to conatct me ✌"}
+          </Description>
+        }
+        extraContent={<IconButtons />}
+      />
+    </Main>
   );
 }
 
