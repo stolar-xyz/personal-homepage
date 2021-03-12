a05076acc50064ea48534b9d4918342f8a4af9b7
Andrzej Stolarek <stolar.code@gmail.com>
2021-03-12 19:17:02 +0100

    deleted IconButtons component

--- src/App/common/IconButtons/styled.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const IconButton = styled.button`
-    margin-right: 24px;
-    color: #ffffff;
-    background: black;
-    cursor: pointer;
-    border: none;
-`;
