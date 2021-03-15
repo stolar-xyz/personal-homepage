c9d6e98812df3d37883486ac9f4fecff6b0be33d
Andrzej Stolarek <stolar.code@gmail.com>
2021-03-15 10:30:35 +0100

    removed some styles from Main and passed to Container

---
+++ src/App/common/Container/index.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+    padding: 14px;
+    margin: 105px 0;
+    display: grid;
+    grid-gap: 72px;
+    grid-template-columns: minmax(auto, 1216px);
+    justify-content: center;
+`;
+
+export default Container;
