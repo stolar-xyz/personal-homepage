28f5c7b607991ff093851474a7ab435d6bf037bf
Andrzej Stolarek <stolar.code@gmail.com>
2021-03-15 10:35:05 +0100

    new application structure with header and footer with deleted articles

--- src/App/App.js
+++ src/App/App.js
@@ -1,15 +1,12 @@
-import Article from "./common/Article";
-import MainLink from "./common/MainLink";
-import Main from "./common/Main";
-import LearnList from "./common/LearnList";
-import Profile from "./common/Profile";
 import Section from "./common/Section";
 import SkillList from "./common/SkillList";
-import TilesSection from "./common/TilesSection";
-import Description from "./common/Description";
+import Tiles from "./common/Tiles";
 import Title from "./common/Title";
-import IconLinks from "./common/IconLinks";
-import ThemeToggler from "./features/themeToggler/ThemeToggler";
+import Footer from "./common/Footer";
+import Container from "./common/Container";
+import Header from "./common/Header";
+import LearnList from "./common/LearnList";
+import Main from "./common/Main";
 import { ThemeProvider } from 'styled-components';
 import { lightTheme, darkTheme } from "./theme";
 import { useSelector } from "react-redux";
@@ -24,73 +21,35 @@ const App = () => {
     <ThemeProvider theme={isThemeDark ? darkTheme : lightTheme}>
       <GlobalStyle />
       <Normalize />
-      <Main>
-        <Article
-          themeToggler={ <ThemeToggler />}
-          image={<Profile />}
-          extraTitleContent={"This is"}
-          title={
-            <Title>
-              {"Andrzej Stolarek"}
-            </Title>
-          }
-          description={
-            <Description>
-              {"👨🏻‍💻 I’m a passionate Frontend Developer in love with React, currently looking for new job opportunities."}
-            </Description>
-          }
-          extraContent={<MainLink />}
-        />
-        <Section
-          title={
-            <Title
-              subtitle
-              as="h2"
-            >
-              {"My skillset includes 🛠️"}
-            </Title>
-          }
-          body={<SkillList />}
-        />
-        <Section
-          title={
-            <Title
-              subtitle
-              as="h2"
-            >
-              {"What I want to learn next 🚀"}
-            </Title>
-          }
-          body={<LearnList />}
-        />
-        <TilesSection
-          title={
-            <Title
-              subtitle
-              as="h2"
-            >
-              {"Portfolio"}
-            </Title>
-          }
-          extraTitleContent={"My recent projects"}
-        />
-        <Article
-          extraTitleContent={"Let’s talk!"}
-          title={
-            <Title
-              mail
-              as="p">
-              {"stolar.code@gmail.com"}
-            </Title>
-          }
-          description={
-            <Description contact>
-              {"I’m always open to new projects whenever I have the time. If you have a website, dashboard or mobile app in mind and need some help to make your ideas come to life, feel free to conatct me ✌"}
-            </Description>
-          }
-          extraContent={<IconLinks />}
-        />
-      </Main>
+      <Container>
+        <Header />
+        <Main>
+          <Section
+            title={
+              <Title
+                subtitle
+                as="h2"
+              >
+                {"My skillset includes 🛠️"}
+              </Title>
+            }
+            body={<SkillList />}
+          />
+          <Section
+            title={
+              <Title
+                subtitle
+                as="h2"
+              >
+                {"What I want to learn next 🚀"}
+              </Title>
+            }
+            body={<LearnList />}
+          />
+          <Tiles />
+        </Main>
+        <Footer />
+      </Container>
     </ThemeProvider>
   );
 };
