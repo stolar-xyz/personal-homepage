4e9a1cc83b889033699e7aed1db82b156515f1d6
Andrzej Stolarek <stolar.code@gmail.com>
2021-03-15 10:31:54 +0100

    added margin-right and deleted styled Container

--- src/App/common/IconLinks/styled.js
+++ src/App/common/IconLinks/styled.js
@@ -1,12 +1,7 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
-    display: grid;
-    grid-template-columns: repeat(4, 1fr);
-    grid-gap: 24px;
-`;
-
 export const IconLink = styled.a`
+    margin-right: 24px;
     color: ${({ theme }) => theme.colors.iconLink};
 
     &:hover {
