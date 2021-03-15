1793c4407850fc6e79da7a9e0c7982cde860a095
Andrzej Stolarek <stolar.code@gmail.com>
2021-03-15 10:30:56 +0100

    changed styles in Description component

--- src/App/common/Description/index.js
+++ src/App/common/Description/index.js
@@ -1,23 +1,21 @@
 import styled, { css } from 'styled-components';
 
 const Description = styled.p`
-    margin: 32px 0;
-    font-size: 20px;
+    font-size: 18px;
+    margin: 24px 0;
     color: ${({ theme }) => theme.colors.primaryText};
     line-height: 1.4;
-    max-width: 633px;
+
+    ${({ header }) => header && css`
+        font-size: 20px;
+        margin: 32px 0;
+        max-width: 633px;
+    `}
 
     ${({ contact }) => contact && css`
         color: ${({ theme }) => theme.colors.secondaryText};;
-        font-size: 18px;
         margin: 0 0 56px 0;
         max-width: 670px;
-    `}
-
-    ${({ tile }) => tile && css`
-        font-size: 18px;
-        margin: 24px 0;
-        max-width: none;
     `}
 `;
 
