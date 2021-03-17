11f11d7afeb03b3f41f01d16862c417bb8b12c8c
Andrzej Stolarek <stolar.code@gmail.com>
2021-03-17 15:41:23 +0100

    added margin to styled StyledProfile component

--- src/App/common/Profile/styled.js
+++ src/App/common/Profile/styled.js
@@ -6,9 +6,10 @@ export const StyledProfile = styled.img`
    max-width: 398px;
    max-height: 398px;
    border-radius: 50%;
-   
+   margin: 0 66px 0 0;
+
    @media (max-width: ${({ theme }) => theme.mobileMax}px) {
       display: block;
-      margin: 0 auto;
+      margin: 0 auto 16px;
    };
 `;
