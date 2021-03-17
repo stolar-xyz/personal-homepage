65c150ed2c317bcafabbf8bcbeeda079b4b882a1
Andrzej Stolarek <stolar.code@gmail.com>
2021-03-17 15:47:00 +0100

    changed Header component structure

--- src/App/common/Header/index.js
+++ src/App/common/Header/index.js
@@ -2,27 +2,28 @@ import React from 'react'
 import ThemeToggler from '../../features/themeToggler/ThemeToggler';
 import Description from '../Description';
 import ExtraTextContent from '../ExtraTextContent';
+import { FlexWrapper } from '../FlexWrapper';
 import MainLink from '../LinkButton';
 import Profile from '../Profile';
 import Title from '../Title';
-import { Container, StyledHeader } from './styled';
+import { StyledHeader } from './styled';
 
 const Header = () => (
     <StyledHeader>
         <ThemeToggler />
         <Profile />
-        <Container>
-            <ExtraTextContent>
-                {"This is"}
-            </ExtraTextContent>
-            <Title header>
-                {"Andrzej Stolarek"}
-            </Title>
-            <Description header>
-                {"👨🏻‍💻 I’m a passionate Frontend Developer in love with React, currently looking for new job opportunities."}
-            </Description>
-            <MainLink />
-        </Container>
+            <FlexWrapper header>
+                <ExtraTextContent>
+                    {"This is"}
+                </ExtraTextContent>
+                <Title header>
+                    {"Andrzej Stolarek"}
+                </Title>
+                <Description header>
+                    {"👨🏻‍💻 I’m a passionate Frontend Developer in love with React, currently looking for new job opportunities."}
+                </Description>
+                <MainLink />
+            </FlexWrapper>
     </StyledHeader>
 );
 
