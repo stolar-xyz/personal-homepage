45acfa02b2fca4baa6f9ef65b56cb83b30077e15
Andrzej Stolarek <stolar.code@gmail.com>
2021-03-17 14:54:08 +0100

    changed Title as paragraph into subheader in Footer

--- src/App/common/Footer/index.js
+++ src/App/common/Footer/index.js
@@ -3,22 +3,23 @@ import Description from '../Description';
 import ExtraTextContent from '../ExtraTextContent'
 import IconLinks from '../IconLinks';
 import Title from '../Title';
+import { StyledFooter } from './styled';
 
 const Footer = () => (
-    <footer>
+    <StyledFooter>
         <ExtraTextContent>
             {"Let’s talk!"}
         </ExtraTextContent>
         <Title
             mail
-            as="p">
+            as="h2">
             {"stolar.code@gmail.com"}
         </Title>
         <Description contact>
             {"I’m always open to new projects whenever I have the time. If you have a website, dashboard or mobile app in mind and need some help to make your ideas come to life, feel free to conatct me ✌"}
         </Description>
         <IconLinks />
-    </footer>
+    </StyledFooter>
 );
 
 export default Footer
