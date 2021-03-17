400e026dd19b3519878c18f0bd127740342f9122
Andrzej Stolarek <stolar.code@gmail.com>
2021-03-17 15:45:04 +0100

    changed component structure and Title subheader to link and added title attribiute

--- src/App/common/Footer/index.js
+++ src/App/common/Footer/index.js
@@ -1,25 +1,28 @@
 import React from 'react'
 import Description from '../Description';
 import ExtraTextContent from '../ExtraTextContent'
+import { FlexWrapper } from '../FlexWrapper';
 import IconLinks from '../IconLinks';
 import Title from '../Title';
-import { StyledFooter } from './styled';
 
 const Footer = () => (
-    <StyledFooter>
+    <FlexWrapper as="footer">
         <ExtraTextContent>
             {"Let’s talk!"}
         </ExtraTextContent>
-        <Title
-            mail
-            as="h2">
-            {"stolar.code@gmail.com"}
-        </Title>
+            <Title
+                mail
+                as="a"
+                href="mailto:stolar.code@gmail.com"
+                title="stolar.code@gmail.com"
+            >
+                {"stolar.code@gmail.com"}
+            </Title>
         <Description contact>
             {"I’m always open to new projects whenever I have the time. If you have a website, dashboard or mobile app in mind and need some help to make your ideas come to life, feel free to conatct me ✌"}
         </Description>
         <IconLinks />
-    </StyledFooter>
+    </FlexWrapper>
 );
 
 export default Footer
