ef02bfa98459305cb14c37052b0eea130834b79f
Andrzej Stolarek <stolar.code@gmail.com>
2021-03-17 15:40:34 +0100

    added title attribiute to LinkButton

--- src/App/common/LinkButton/index.js
+++ src/App/common/LinkButton/index.js
@@ -2,7 +2,10 @@ import React from 'react'
 import { StyledLinkButton, LinkButtonIcon } from './styled';
 
 const LinkButton = () => (
-    <StyledLinkButton href="mailto:stolar.code@gmail.com">
+    <StyledLinkButton
+        href="mailto:stolar.code@gmail.com"
+        title="stolar.code@gmail.com"
+    >
         <LinkButtonIcon /> Hire Me
     </StyledLinkButton>
 );
