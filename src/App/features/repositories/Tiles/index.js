b14ad732a42273cf4cf72163557c5df2376dfecd
Andrzej Stolarek <stolar.code@gmail.com>
2021-03-22 16:02:34 +0100

    added tiles with data from repos and Loading and Failure components depending on repositories status

--- src/App/features/repositories/Tiles/index.js
+++ src/App/features/repositories/Tiles/index.js
@@ -1,49 +1,54 @@
 import React from 'react'
 import Tile from '../Tile';
-import { Container, Wrapper, ExtraTitleContent, GithubIcon, StyledTiles } from './styled';
+import Loading from "../Loading";
+import Failure from '../Failure';
 import Title from '../../../common/Title';
+import { Container, Wrapper, ExtraTitleContent, GithubIcon, StyledTiles } from './styled';
+import { useSelector } from "react-redux";
+import { selectContent, selectStatus } from '../repositoriesSlice';;
 
-const Tiles = () => (
-    <StyledTiles>
-        <Wrapper>
-            <GithubIcon />
-            <Title
-                subtitle
-                as="h2"
-            >
-                {"Portfolio"}
-            </Title>
-            <ExtraTitleContent>
-                {"My recent projects"}
-            </ExtraTitleContent>
-        </Wrapper>
-        <Container>
-            <Tile
-                header={"Movies Browser"}
-                description={"Project description, e.g. website where you can search for favourite movies and people. Project description, e.g. website where you can search."}
-                demo={"https://link.demo.com"}
-                code={"https://link.code.com"}
-            />
-            <Tile
-                header={"Movies Browser"}
-                description={"Project description, e.g. website where you can search for favourite movies and people. Project description, e.g. website where you can search."}
-                demo={"https://link.demo.com"}
-                code={"https://link.code.com"}
-            />
-            <Tile
-                header={"Movies Browser"}
-                description={"Project description, e.g. website where you can search for favourite movies and people. Project description, e.g. website where you can search."}
-                demo={"https://link.demo.com"}
-                code={"https://link.code.com"}
-            />
-            <Tile
-                header={"Movies Browser"}
-                description={"Project description, e.g. website where you can search for favourite movies and people. Project description, e.g. website where you can search."}
-                demo={"https://link.demo.com"}
-                code={"https://link.code.com"}
-            />
-        </Container>
-    </StyledTiles>
-);
+const Tiles = () => {
+    const repositoriesStatus = useSelector(selectStatus);
+    const repositories = useSelector(selectContent);
+
+    return (
+        <StyledTiles>
+            <Wrapper>
+                <GithubIcon />
+                <Title
+                    subtitle
+                    as="h2"
+                >
+                    {"Portfolio"}
+                </Title>
+                <ExtraTitleContent>
+                    {"My recent projects"}
+                </ExtraTitleContent>
+            </Wrapper>
+            {repositoriesStatus === "loading"
+                ? (
+                    <Loading />
+                )
+                : repositoriesStatus === "error"
+                    ? (
+                        <Failure />
+                    )
+                    : (
+                        <Container>
+                            {repositories.map(repository => (
+                                <Tile
+                                    key={repository.id}
+                                    header={repository.name}
+                                    description={repository.description}
+                                    demo={repository.homepage}
+                                    code={repository.html_url}
+                                />
+                            ))}
+                        </Container>
+                    )
+            }
+        </StyledTiles>
+    );
+};
 
 export default Tiles;
