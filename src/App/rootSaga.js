67d5849fbf2df9f5f47e86c374bb0fcf0eb6461c
Andrzej Stolarek <stolar.code@gmail.com>
2021-03-22 15:47:08 +0100

    added repositoriesSaga to root

--- src/App/rootSaga.js
+++ src/App/rootSaga.js
@@ -1,8 +1,10 @@
 import { all } from "redux-saga/effects";
+import { repositoriesSaga } from "./features/repositories/repositoriesSaga";
 import { themeSaga } from "./features/themeToggler/themeSaga"
 
 export default function* rootSaga() {
     yield all([
         themeSaga(),
-    ])
+        repositoriesSaga(),
+    ]);
 };
