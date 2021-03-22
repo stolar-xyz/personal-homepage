3e0c6059f5d53347083cfc07c9bc615a36c3ce11
Andrzej Stolarek <stolar.code@gmail.com>
2021-03-22 15:45:25 +0100

    added repositoriesSlice to store

--- src/App/store.js
+++ src/App/store.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import createSagaMiddleware from "redux-saga";
 import rootSaga from "./rootSaga";
 import themeReducer from "./features/themeToggler/themeSlice";
+import repositoriesReducer from "./features/repositories/repositoriesSlice";
 
 const sagaMiddleware = createSagaMiddleware();
 
 const store = configureStore({
     reducer: {
         theme: themeReducer,
+        repositories: repositoriesReducer,
     },
     middleware: [sagaMiddleware]
 });
