245ff69151bebb56fe3462dafad8f0c02b08e8b7
Andrzej Stolarek <stolar.code@gmail.com>
2021-03-23 20:19:40 +0100

    added media queries to Loading component

--- src/App/features/repositories/Loading/styled.js
+++ src/App/features/repositories/Loading/styled.js
@@ -4,7 +4,7 @@ export const StyledLoading = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
-    margin-top: 88px;
+    margin: 88px 0 140px;
 `;
 
 export const Description = styled.p`
@@ -13,6 +13,11 @@ export const Description = styled.p`
     color: ${({ theme }) => theme.color.text.secondaryText};
     line-height: 1.4;
     text-align: center;
+
+    @media (max-width: ${({ theme }) => theme.mobileMax}px) {
+        font-size: 14px;
+        margin: 0 0 32px 0;
+    };
 `;
 
 const spin = keyframes`
@@ -32,4 +37,10 @@ export const Loader = styled.div`
     border: 10px solid ${({ theme }) => theme.color.border.loaderBorder};
     border-top-color: #2B77CC;
     animation: ${spin} 1s linear infinite;
+
+    @media (max-width: ${({ theme }) => theme.mobileMax}px) {
+        width: 130px;
+        height: 130px; 
+        border-width: 5px;
+    };
 `;
