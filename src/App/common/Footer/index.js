cb0c59d1ebc079e8e2c1b7525244e7efd6d7488e
Andrzej Stolarek <stolar.code@gmail.com>
2021-03-25 13:06:34 +0100

    added styled components and changed Footer structure

--- src/App/common/Footer/index.js
+++ src/App/common/Footer/index.js
@@ -1,28 +1,24 @@
 import React from 'react'
-import Description from '../Description';
 import ExtraTextContent from '../ExtraTextContent'
 import { FlexWrapper } from '../FlexWrapper';
 import IconLinks from '../IconLinks';
-import Title from '../Title';
+import { Description, Email } from './styled';
 
 const Footer = () => (
     <FlexWrapper as="footer">
         <ExtraTextContent>
             Let’s talk!
         </ExtraTextContent>
-        <Title
-            mail
-            as="a"
+        <Email
             href="mailto:stolar.code@gmail.com"
-            title="stolar.code@gmail.com"
-        >
+            title="stolar.code@gmail.com">
             stolar.code@gmail.com
-            </Title>
-        <Description contact>
+        </Email>
+        <Description>
             I’m always open to new projects whenever I have the time. If you have a website, dashboard or mobile app in mind and need some help to make your ideas come to life, feel free to conatct me ✌
         </Description>
         <IconLinks />
     </FlexWrapper>
 );
 
-export default Footer
+export default Footer;
---
+++ src/App/common/Footer/styled.js
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+
+export const Description = styled.p`
+    font-size: 18px;
+    line-height: 1.4;
+    color: ${({ theme }) => theme.color.text.secondaryText};
+    margin: 0 0 56px 0;
+    max-width: 670px;
+
+    @media (max-width: ${({ theme }) => theme.mobileMax}px) {
+        font-size: 14px;
+        margin: 0 0 40px 0;;
+    };
+`;
+
+export const Email = styled.a`
+    color: ${({ theme }) => theme.color.text.title};
+    font-weight: 900;
+    font-size: 32px;
+    margin: 24px 0;
+    transition: color 0.3s;
+    text-decoration: none;
+
+    &:hover {
+        color: ${({ theme }) => theme.color.hover.title};
+    };
+
+    @media (max-width: ${({ theme }) => theme.mobileMax}px) {
+        font-size: 18px;
+        margin: 12px 0;
+    };
+`;
