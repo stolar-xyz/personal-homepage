25d21d1711a38904c0c0013cfc69a95358d1ecef
Andrzej Stolarek <stolar.code@gmail.com>
2021-03-25 01:00:56 +0100

    added Subheader component

---
+++ src/App/common/Subheader/index.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const Subheader = styled.h2`
+    color: ${({ theme }) => theme.color.text.title};
+    font-weight: 900;
+    font-size: 30px;
+    margin: 0;
+
+    @media (max-width: ${({ theme }) => theme.mobileMax}px) {
+        font-size: 18px;
+    };
+`;
+
+export default Subheader;
