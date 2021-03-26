14fdb37b1bdd1fd50b4e86ce386d50099cdbc8ca
Andrzej Stolarek <stolar.code@gmail.com>
2021-03-26 15:13:10 +0100

    changed IconLinks structure by mapping socials

--- src/App/common/structure/IconLinks/index.js
+++ src/App/common/structure/IconLinks/index.js
@@ -1,41 +1,22 @@
 import React from 'react'
 import { IconLink, StyledIconLinks } from './styled';
-import { ReactComponent as GithubExtraIcon } from "../../../images/svgs/githubExtra.svg";
-import { ReactComponent as FacebookIcon } from "../../../images/svgs/facebook.svg";
-import { ReactComponent as LinkedInIcon } from "../../../images/svgs/linkedIn.svg";
-import { ReactComponent as InstagramIcon } from "../../../images/svgs/instagram.svg";
+import { socials } from '../../../personalInfo';
+
 
 const IconLinks = () => (
     <StyledIconLinks>
-        <IconLink
-            href="https://github.com/stolar-code/"
-            target="_blank"
-            rel="noopener noreferrer"
-            title="Github">
-            <GithubExtraIcon />
-        </IconLink>
-        <IconLink
-            href="https://www.facebook.com/ctucb/"
-            target="_blank"
-            rel="noopener noreferrer"
-            title="Facebook">
-            <FacebookIcon />
-        </IconLink>
-        <IconLink
-            href="https://www.linkedin.com/in/stolar-code/"
-            target="_blank"
-            rel="noopener noreferrer"
-            title="LinkedIn">
-            <LinkedInIcon />
-        </IconLink>
-        <IconLink
-            href="https://www.instagram.com/stolar.xyz/"
-            target="_blank"
-            rel="noopener noreferrer"
-            title="Instagram">
-            <InstagramIcon />
-        </IconLink>
+        {socials.map(({ link, title, Icon }) => (
+            <IconLink
+                href={link}
+                title={title}
+                key={title}
+                target="_blank"
+                rel="noopener noreferrer">
+                <Icon />
+            </IconLink>
+        ))}
     </StyledIconLinks>
 );
 
 export default IconLinks;
+
