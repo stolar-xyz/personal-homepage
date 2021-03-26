c3f91eb855271c49089644d6c9da6f7473fc50a6
Andrzej Stolarek <stolar.code@gmail.com>
2021-03-26 15:20:50 +0100

    added List component with mapping passed items

---
+++ src/App/common/structure/List/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { ListItem, StyledList } from './styled';
+
+const List = ({ items }) => (
+    <StyledList>
+        {items.map(item => (
+            <ListItem key={item}>
+                {item}
+            </ListItem>
+        ))}
+    </StyledList>
+);
+
+export default List;
