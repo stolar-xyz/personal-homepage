5d4a6ba3d9660a6f6ad595a7e4859dd2a2679b21
Andrzej Stolarek <stolar.code@gmail.com>
2021-03-27 22:14:56 +0100

    updated footer description

--- src/App/common/structure/Footer/index.js
+++ src/App/common/structure/Footer/index.js
@@ -15,9 +15,9 @@ const Footer = () => (
             {email}
         </Email>
         <Description>
-            I’m always open to new projects whenever I have the time. If you have a website,
-            dashboard or mobile app in mind and need some help to make your ideas come to life,
-            feel free to conatct me ✌
+            I’m always open to new projects whenever I have the time. If you have a website
+            in mind and need some help to make your ideas come to life,
+            feel free to contact me ✌
         </Description>
         <IconLinks>
             {socials.map(({ link, title, Icon }) => (
