1918bf1bd6e09fea95aa73ac60547db61f39c0dd
Andrzej Stolarek <stolar.code@gmail.com>
2021-03-29 14:31:48 +0200

    changed Container to ul with additional styles

--- src/App/features/repositories/Tiles/styled.js
+++ src/App/features/repositories/Tiles/styled.js
@@ -26,11 +26,13 @@ export const ExtraSubheaderContent = styled.p`
     };
 `;
 
-export const Container = styled.div`
+export const Container = styled.ul`
     display: grid;
     grid-template-columns: repeat(2, 1fr);
     grid-gap: 32px;
     margin: 24px 0 140px;
+    padding: 0;
+    list-style: none;
 
     @media (max-width: ${({ theme }) => theme.tabletMax}px) {
         grid-gap: 18px;
