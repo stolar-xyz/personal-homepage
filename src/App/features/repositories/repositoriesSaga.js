69e881a6715cbd460e687a3e51cdc8af8447e646
Andrzej Stolarek <stolar.code@gmail.com>
2021-03-29 13:30:31 +0200

    added comment to delay line

--- src/App/features/repositories/repositoriesSaga.js
+++ src/App/features/repositories/repositoriesSaga.js
@@ -4,7 +4,7 @@ import { setRepositories, setStatus } from "./repositoriesSlice";
 
 function* fetchRepositoriesHandler() {
     try {
-        yield delay(2000);
+        yield delay(2000); // just to demo the loading
         const repositories = yield call(getRepositories);
         yield put(setRepositories(repositories));
         yield put(setStatus("success"));
