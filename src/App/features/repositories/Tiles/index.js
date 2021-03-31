bfa0b1e3e16ec78749eca8c8e6513b735a5d8bee
Andrzej Stolarek <stolar.code@gmail.com>
2021-03-31 23:16:32 +0200

    added Success component to structure and deleted useless things

--- src/App/features/repositories/Tiles/index.js
+++ src/App/features/repositories/Tiles/index.js
@@ -1,18 +1,17 @@
 import React, { useEffect } from 'react'
-import Tile from '../Tile';
 import Loading from "../Loading";
 import Failure from '../Failure';
+import Success from '../Success';
 import Subheader from '../../../common/styled/Subheader';
-import { Container, Wrapper, ExtraSubheaderContent, Icon } from './styled';
+import { Wrapper, ExtraSubheaderContent, Icon } from './styled';
 import { useDispatch, useSelector } from "react-redux";
-import { fetchRepositories, selectRepositories, selectStatus } from '../repositoriesSlice';
+import { fetchRepositories, selectStatus } from '../repositoriesSlice';
 
 const Tiles = () => {
     const dispatch = useDispatch();
 
     const repositoriesStatus = useSelector(selectStatus);
-    const repositories = useSelector(selectRepositories);
-
+    
     useEffect(() => {
         dispatch(fetchRepositories());
     }, [dispatch]);
@@ -37,19 +36,7 @@ const Tiles = () => {
                     case "error":
                         return <Failure />;
                     case "success":
-                        return (
-                            <Container>
-                                {repositories.map(({ id, name, description, homepage, html_url }) => (
-                                    <Tile
-                                        key={id}
-                                        title={name}
-                                        description={description}
-                                        demo={homepage}
-                                        code={html_url}
-                                    />
-                                ))}
-                            </Container>
-                        );
+                        return <Success />;
                     default:
                         throw new Error(`incorrect status - ${repositoriesStatus}`);
                 }
