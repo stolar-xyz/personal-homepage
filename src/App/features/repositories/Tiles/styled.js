9834e3bfc279c5d55c80da29b23e6000c7baf4c2
Andrzej Stolarek <stolar.code@gmail.com>
2021-03-31 23:16:52 +0200

    deleted Container styled component

--- src/App/features/repositories/Tiles/styled.js
+++ src/App/features/repositories/Tiles/styled.js
@@ -25,17 +25,3 @@ export const ExtraSubheaderContent = styled.p`
         margin: 16px 0 0;
     };
 `;
-
-export const Container = styled.ul`
-    display: grid;
-    grid-template-columns: repeat(2, 1fr);
-    grid-gap: 32px;
-    margin: 24px 0 0;
-    padding: 0;
-    list-style: none;
-
-    @media (max-width: ${({ theme }) => theme.tabletMax}px) {
-        grid-gap: 18px;
-        grid-template-columns: 1fr;
-    };
-`;
