e275472c1f81e5494a6090adb642a2b62cd7b8a5
Andrzej Stolarek <stolar.code@gmail.com>
2021-03-31 22:52:28 +0200

    changed conditional rendering to switch with new values and added useEffect with fetchRepositoreis dispatch

--- src/App/features/repositories/Tiles/index.js
+++ src/App/features/repositories/Tiles/index.js
@@ -1,15 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Tile from '../Tile';
 import Loading from "../Loading";
 import Failure from '../Failure';
 import Subheader from '../../../common/styled/Subheader';
 import { Container, Wrapper, ExtraSubheaderContent, Icon } from './styled';
-import { useSelector } from "react-redux";
-import { selectRepositories, selectStatus } from '../repositoriesSlice';
+import { useDispatch, useSelector } from "react-redux";
+import { fetchRepositories, selectRepositories, selectStatus } from '../repositoriesSlice';
 
 const Tiles = () => {
+    const dispatch = useDispatch();
+
     const repositoriesStatus = useSelector(selectStatus);
     const repositories = useSelector(selectRepositories);
+
+    useEffect(() => {
+        dispatch(fetchRepositories());
+    }, [dispatch]);
 
     return (
         <section>
@@ -22,28 +28,32 @@ const Tiles = () => {
                     My recent projects
                 </ExtraSubheaderContent>
             </Wrapper>
-            {repositoriesStatus === "loading"
-                ? (
-                    <Loading />
-                )
-                : repositoriesStatus === "error"
-                    ? (
-                        <Failure />
-                    )
-                    : (
-                        <Container>
-                            {repositories.map(({ id, name, description, homepage, html_url }) => (
-                                <Tile
-                                    key={id}
-                                    title={name}
-                                    description={description}
-                                    demo={homepage}
-                                    code={html_url}
-                                />
-                            ))}
-                        </Container>
-                    )
-            }
+            {(() => {
+                switch (repositoriesStatus) {
+                    case "initial":
+                        return null;
+                    case "loading":
+                        return <Loading />;
+                    case "error":
+                        return <Failure />;
+                    case "success":
+                        return (
+                            <Container>
+                                {repositories.map(({ id, name, description, homepage, html_url }) => (
+                                    <Tile
+                                        key={id}
+                                        title={name}
+                                        description={description}
+                                        demo={homepage}
+                                        code={html_url}
+                                    />
+                                ))}
+                            </Container>
+                        );
+                    default:
+                        throw new Error(`incorrect status - ${repositoriesStatus}`);
+                }
+            })()}
         </section>
     );
 };
