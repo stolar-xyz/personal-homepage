4862f226f03cc6bd7a677397c9c1d27255f8afd2
Andrzej Stolarek <stolar.code@gmail.com>
2021-03-31 17:28:41 +0200

    added secondaryText color inheritance from StyledFailure

--- src/App/features/repositories/Failure/styled.js
+++ src/App/features/repositories/Failure/styled.js
@@ -3,13 +3,13 @@ import { ReactComponent as dangerIcon } from "../../../images/svgs/danger.svg";
 
 export const StyledFailure = styled.div`
     text-align: center;
+    color: ${({ theme }) => theme.color.text.secondaryText};
     margin-top: 96px;
 `;
 
 export const Header = styled.h3`
     font-size: 24px;
     margin: 24px 0 0 0;
-    color: ${({ theme }) => theme.color.text.secondaryText};
 
     @media (max-width: ${({ theme }) => theme.mobileMax}px) {
         font-size: 16px;
@@ -19,7 +19,6 @@ export const Header = styled.h3`
 export const Description = styled.p`
     font-size: 20px;
     margin: 32px 0;
-    color: ${({ theme }) => theme.color.text.secondaryText};
     line-height: 1.4;
 
     @media (max-width: ${({ theme }) => theme.mobileMax}px) {
