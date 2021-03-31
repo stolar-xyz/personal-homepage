6dac728587fcfac78b7837f8c3f9850d0d09832b
Andrzej Stolarek <stolar.code@gmail.com>
2021-03-31 23:27:42 +0200

    changed Repositories structure

--- src/App/features/repositories/Repositories/index.js
+++ src/App/features/repositories/Repositories/index.js
@@ -1,48 +1,21 @@
-import React, { useEffect } from 'react'
-import Loading from "../Loading";
-import Failure from '../Failure';
-import Success from '../Success';
+import React from 'react'
 import Subheader from '../../../common/styled/Subheader';
+import Content from '../Content';
 import { Wrapper, ExtraSubheaderContent, Icon } from './styled';
-import { useDispatch, useSelector } from "react-redux";
-import { fetchRepositories, selectStatus } from '../repositoriesSlice';
 
-const Repositories = () => {
-    const dispatch = useDispatch();
-
-    const repositoriesStatus = useSelector(selectStatus);
-    
-    useEffect(() => {
-        dispatch(fetchRepositories());
-    }, [dispatch]);
-
-    return (
-        <section>
-            <Wrapper>
-                <Icon />
-                <Subheader>
-                    Portfolio
-                </Subheader>
-                <ExtraSubheaderContent>
-                    My recent projects
-                </ExtraSubheaderContent>
-            </Wrapper>
-            {(() => {
-                switch (repositoriesStatus) {
-                    case "initial":
-                        return null;
-                    case "loading":
-                        return <Loading />;
-                    case "error":
-                        return <Failure />;
-                    case "success":
-                        return <Success />;
-                    default:
-                        throw new Error(`incorrect status - ${repositoriesStatus}`);
-                }
-            })()}
-        </section>
-    );
-};
+const Repositories = () => (
+    <section>
+        <Wrapper>
+            <Icon />
+            <Subheader>
+                Portfolio
+            </Subheader>
+            <ExtraSubheaderContent>
+                My recent projects
+            </ExtraSubheaderContent>
+        </Wrapper>
+        <Content />
+    </section>
+);
 
 export default Repositories;
