09372d376364ec741f5d2bc1a3624d5503b6e388
Andrzej Stolarek <stolar.code@gmail.com>
2021-04-01 18:15:35 +0200

    formatted Failure component

--- src/App/features/repositories/Content/Failure/index.js
+++ src/App/features/repositories/Content/Failure/index.js
@@ -1,26 +1,26 @@
-import React from 'react'
+import React from 'react';
 import ButtonLink from '../../../../common/styled/ButtonLink';
 import { DangerIcon, Description, Header, StyledFailure } from './styled';
 import { repositoriesUrl } from '../../../../personalInfo';
 
 const Failure = () => (
-        <StyledFailure>
-            <DangerIcon />
-            <Header>
-                Ooops! Something went wrong...
-            </Header>
-            <Description>
-                Sorry, failed to load projects. <br />
-                You can check them directly on GitHub.
-            </Description>
-            <ButtonLink
-                href={repositoriesUrl}
-                target="_blank"
-                rel="noopener noreferrer"
-                title="GitHub repositories">
-                Go to GitHub
-            </ButtonLink>
-        </StyledFailure >
+    <StyledFailure>
+        <DangerIcon />
+        <Header>
+            Ooops! Something went wrong...
+        </Header>
+        <Description>
+            Sorry, failed to load projects. <br />
+            You can check them directly on GitHub.
+        </Description>
+        <ButtonLink
+            href={repositoriesUrl}
+            target="_blank"
+            rel="noopener noreferrer"
+            title="GitHub repositories">
+            Go to GitHub
+        </ButtonLink>
+    </StyledFailure >
 );
 
 export default Failure;
